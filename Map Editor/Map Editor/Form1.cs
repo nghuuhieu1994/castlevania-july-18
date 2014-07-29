904c77debb24546b4bf44a0132a47410daf695d2
nghuuhieu1994 <nghuuhieu1994@fde82c1b-8767-f34d-64bb-2fc816664c25>
2014-07-29 23:50:45 +0000

    [Hieu] update ME

--- Map Editor/Map Editor/Form1.cs
+++ Map Editor/Map Editor/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,7 @@ namespace Map_Editor
             pictureBox1.Image = map;
             Button _newButton = new Button();
 
-            _newButton.BackgroundImage = new Bitmap("simon.png");
+            _newButton.BackgroundImage = new Bitmap("2.png");
 
             _newButton.Size = new System.Drawing.Size(64, 64);
             _newButton.BackgroundImageLayout = ImageLayout.Zoom;
@@ -39,7 +40,7 @@ namespace Map_Editor
             string _output = "X: " + mouse_location.X + " Y: " + mouse_location.Y;
             //Bitmap img = new Bitmap("background1.png");
             Graphics g = Graphics.FromImage(Main_Image);
-            g.DrawImage(Image.FromFile("simon.png"), mouse_location);
+            g.DrawImage(Image.FromFile("2.png"), mouse_location);
             pictureBox1.Image = Main_Image;
             //img.Save("background1.png");
             Console.WriteLine(_output);
@@ -48,12 +49,22 @@ namespace Map_Editor
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
             Point mouse_location = pictureBox1.PointToClient(Control.MousePosition);
-            //string _output = "X: " + mouse_location.X + " Y: " + mouse_location.Y;
-            //Bitmap temp = new Bitmap("background1.png");
-            Image temp = (Image)Main_Image.Clone();
             
+            Image temp = (Image)Main_Image.Clone();
+
+            ColorMatrix matrix = new ColorMatrix(); 
+            matrix.Matrix33 = 0.7f;
+
+            
+            ImageAttributes attributes = new ImageAttributes();
+
+            
+            attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);    
+
             Graphics g = Graphics.FromImage(temp);
-            g.DrawImage(Image.FromFile("simon.png"), mouse_location);
+            Image icon = Image.FromFile("2.png"); 
+            //g.DrawImage(icon, mouse_location,
+            g.DrawImage(icon, new Rectangle(mouse_location, new Size(icon.Width, icon.Height)), 0, 0, icon.Width, icon.Height, GraphicsUnit.Pixel, attributes);
 
             pictureBox1.Image = temp;
             temp = null;
